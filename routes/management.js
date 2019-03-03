75575cc769379090ffe3aad67f0baf6276828cd4
GinOnPC <antoinecoulonponslet@gmail.com>
2019-03-03 20:46:00 +0100

    Moved script.js

--- routes/management.js
+++ routes/management.js
@@ -17,7 +17,6 @@ router.get('/newkey', isAuthenticated, function (req, res, next) {
     res.send({ key1: key1, key2: key2, key3: key3 });
   });
 
-
 });
 
 
@@ -26,7 +25,6 @@ function isAuthenticated(req, res, next) {
   if (req.session.user)
     return next();
 
-  // IF A USER ISN'T LOGGED IN, THEN REDIRECT THEM SIGNIN PAGE
   res.redirect('/signin');
 }
 
